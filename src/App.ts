e0d9420659e452bd82d0579000a5de9fb7b5cc6d
Benji Sloan <sloanrobe@hotmail.com>
2025-02-14 13:42:14 +0100

    Added foundational code structure

--- src/App.ts
+++ src/App.ts
@@ -1,12 +1,20 @@
-import "dotenv.config";
+import "dotenv/config";
 import TicketMachine from "./TicketMachine";
 
 class App {
     main() {
         const currency = process.env.CURRENCY
         const pricePerMinute = process.env.PRICE_PER_MINUTE
-        const ticketmachine = new TicketMachine()
         
+    if(!currency || !pricePerMinute) {
+        throw new Error("Error in configuration");   
+    }
+
+    //Instantiation
+    const ticketmachine = new TicketMachine(currency, Number(pricePerMinute))
+    //new TicketMachine("EUR", 2).startInteraction()
+    ticketmachine.startInteraction()
+    ticketmachine.endInteraction()
     }
 }
 
--- src/TicketMachine.ts
+++ src/TicketMachine.ts
@@ -1,9 +1,43 @@
+import {rawlist} from '@inquirer/prompts'
+
 export default class TicketMachine {
     private currency: string
     private pricePerMinute: number
 
     constructor(_currency: string, _pricePerMinute: number) {
         this.currency = _currency
-        this.pricePerMinute = _pricePerMinute
+        this.pricePerMinute = _pricePerMinute    
     }
-}
+
+    async startInteraction() {
+        const duration = await this.askForDurationInConsole()
+        const price = this.calculatePrice(duration)
+        this.displayPrice(price)
+        }
+
+    endInteraction() {
+        console.log('Thank you for parking with us!')
+    }
+
+    private displayPrice(price: number) {
+        console.log(`The price will be ${price}${this.currency}.`)
+    }
+    
+    private calculatePrice(duration: number) {
+        return duration * this.pricePerMinute
+    }
+
+    private async askForDurationInConsole() {
+        const answer = await rawlist({
+            message: "Select a duration",
+            choices: [
+                { name: "30 minutes", value: 30 },
+                { name: "1 hour", value: 60 },
+                { name: "2 hours", value: 120 },
+                { name: "4 hours", value: 2400 },
+                { name: "8 hours", value: 480 },
+            ],
+        });
+        return answer
+    }
+    }
